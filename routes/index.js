67efbb0c63ee25ad00ad09b0e23884506de3f6cf
Alexey <alexey.ernest@gmail.com>
2015-11-05 18:18:30 +0600

    Indents.

--- routes/index.js
+++ routes/index.js
@@ -1,47 +1,52 @@
 var debug = require('debug')('clickberry:profiles:api');
 
 exports.notfound = function (req, res) {
-    res.status(404).format({
-        html: function () {
-            res.render('404', { title: '404 Not Found'});
-        },
-        json: function () {
-            res.send({ message: 'Resource not found' });
-        },
-        xml: function () {
-            res.write('<error>\n');
-            res.write('    <message>Resource not found</message>\n');
-            res.end('</error>\n');
-        },
-        text: function () {
-            res.send('Resource not found\n');
-        }
-    });
+	res.status(404).format({
+		html: function () {
+			res.render('404', { title: '404 Not Found'});
+		},
+		json: function () {
+			res.send({ message: 'Resource not found' });
+		},
+		xml: function () {
+			res.write('<error>\n');
+			res.write('    <message>Resource not found</message>\n');
+			res.end('</error>\n');
+		},
+		text: function () {
+			res.send('Resource not found\n');
+		}
+	});
 };
 
 exports.error = function (err, req, res, next) {
-    console.log(err.stack);
-    var msg;
+	console.log(err.stack);
+	var msg;
 
-    switch (err.type) {
-        case 'database':
-            msg = 'Service Unavailable';
-            res.statusCode = 503;
-            break;
-        default:
-            msg = 'Internal Server Error';
-            res.statusCode = 500;
-    }
+	switch (err.type) {
+		case 'database':
+			msg = 'Service Unavailable';
+			res.statusCode = 503;
+			break;
+		default:
+			msg = 'Internal Server Error';
+			res.statusCode = 500;
+	}
 
-    res.format({
-        html: function () {
-            res.render('5xx', { title: msg, msg: msg, status: res.statusCode });
-        },
-        json: function () {
-            res.send({ error: msg });
-        },
-        text: function () {
-            res.send(msg + '\n');
-        }
-    });
+	res.format({
+		html: function () {
+			res.render('5xx', { title: msg, msg: msg, status: res.statusCode });
+		},
+		json: function () {
+			res.send({ error: msg });
+		},
+		xml: function () {
+      res.write('<error>\n');
+      res.write('    <message>' + msg + '</message>\n');
+      res.end('</error>\n');
+    },
+		text: function () {
+			res.send(msg + '\n');
+		}
+	});
 };
